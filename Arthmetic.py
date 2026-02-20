3d66b078039cb1e8a1de3d6cd1dddb19dc10863d
Mr-J-Debug <jignesh.jain589@gmail.com>
2026-02-20 11:27:54 +0530

    python assignments

---
+++ Arthmetic.py
@@ -0,0 +1,8 @@
+test1 = input("Enter the 1st number: ")
+test2 = input("Enter the 2nd number: ")
+
+
+print(int(test1) + int(test2))
+print(int(test1) - int(test2))
+print(int(test1) * int(test2))
+print(int(test1) / int(test2))
---
+++ Name.py
@@ -0,0 +1,4 @@
+Name = input("Enter your First name: ")
+surname = input("Enter your Last name: ")
+
+print("Hello, " + Name + " " + surname + " " + "Welcome to the python program")
